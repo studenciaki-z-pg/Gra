d2e45e1fe8c9ed5320ae273dde9a1b3392d454f0
EveryUsernameAlreadyTaken <kamila.kaliszewicz@gmail.com>
2019-05-07 18:59:55 +0200

    Hex Map#1
    Generowanie mapy i rozmyte granice pól
    +usunięcie poprzednich map

---
+++ PodrozwNieznane/Assets/Scripts/HexGrid.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+    HexCell[] cells;
+
+    public Text cellLabelPrefab;
+    Canvas gridCanvas;
+
+    HexMesh hexMesh;
+
+    public Color defaultColor = Color.white;
+    public Color touchedColor = Color.magenta;
+    public Color[] colors;
+
+    void Awake()
+    {
+        gridCanvas = GetComponentInChildren<Canvas>();
+        hexMesh = GetComponentInChildren<HexMesh>();
+
+        gridCanvas = GetComponentInChildren<Canvas>();
+        cells = new HexCell[height * width];
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        //cell.color = defaultColor;
+        cell.color = colors[Random.Range(0, 4)];
+
+        if (x > 0) {
+            cell.SetNeighbor(HexDirection.W, cells[i - 1]);
+        }
+        if (z > 0) {
+            if ((z & 1) == 0) {
+                cell.SetNeighbor(HexDirection.SE, cells[i - width]);
+                if (x > 0) {
+                    cell.SetNeighbor(HexDirection.SW, cells[i - width - 1]);
+                }
+            }
+            else {
+                cell.SetNeighbor(HexDirection.SW, cells[i - width]);
+                if (x < width - 1) {
+                    cell.SetNeighbor(HexDirection.SE, cells[i - width + 1]);
+                }
+            }
+        }
+
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+    }
+
+    void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            HandleInput();
+        }
+    }
+
+    void HandleInput()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit))
+        {
+            TouchCell(hit.point);
+        }
+    }
+
+    void TouchCell(Vector3 position)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        Debug.Log("touched at " + coordinates.ToString());
+
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        //cell.color = touchedColor;
+        hexMesh.Triangulate(cells);
+    }
+
+}
---
+++ PodrozwNieznane/Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexMetrics
+{
+    public const float outerRadius = 10f;
+    public const float innerRadius = outerRadius * 0.866025404f; //(sqrt(3)/2)
+
+    public const float solidFactor = 0.75f;
+    public const float blendFactor = 1f - solidFactor;
+
+    // flat top orientation:
+    /*public static Vector3[] corners = {
+        new Vector3(0.5f * outerRadius, 0f, innerRadius),
+        new Vector3(outerRadius, 0f, 0f),
+        new Vector3(0.5f * outerRadius, 0f, -innerRadius),
+        new Vector3(-0.5f * outerRadius, 0f, -innerRadius),
+        new Vector3(-outerRadius, 0f, 0f),
+        new Vector3(-0.5f * outerRadius, 0f, innerRadius),
+        new Vector3(0.5f * outerRadius, 0f, innerRadius), //"seventh" corner - duplicate of first
+    };
+    */
+    // pointy top orientation:
+    static Vector3[] corners = {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(0f, 0f, outerRadius) //"seventh" corner - duplicate of first
+    };
+
+    public static Vector3 GetFirstCorner(HexDirection direction)
+    {
+        return corners[(int)direction];
+    }
+
+    public static Vector3 GetSecondCorner(HexDirection direction)
+    {
+        return corners[(int)direction + 1];
+    }
+    public static Vector3 GetFirstSolidCorner(HexDirection direction)
+    {
+        return corners[(int)direction] * solidFactor;
+    }
+
+    public static Vector3 GetSecondSolidCorner(HexDirection direction)
+    {
+        return corners[(int)direction + 1] * solidFactor;
+    }
+
+    public static Vector3 GetBridge(HexDirection direction)
+    {
+        return (corners[(int)direction] + corners[(int)direction + 1]) *
+            blendFactor;
+    }
+
+}
+
+
+public enum HexDirection
+{
+    NE, E, SE, SW, W, NW
+}
+
+public static class HexDirectionExtensions
+{
+
+    public static HexDirection Opposite(this HexDirection direction)
+    {
+        return (int)direction < 3 ? (direction + 3) : (direction - 3);
+    }
+
+    public static HexDirection Previous(this HexDirection direction)
+    {
+        return direction == HexDirection.NE ? HexDirection.NW : (direction - 1);
+    }
+
+    public static HexDirection Next(this HexDirection direction)
+    {
+        return direction == HexDirection.NW ? HexDirection.NE : (direction + 1);
+    }
+
+
+}
