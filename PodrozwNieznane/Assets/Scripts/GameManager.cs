c96fa752a45faa01b975b6f466cd1f4f976ec2a7
EveryUsernameAlreadyTaken <kamila.kaliszewicz@gmail.com>
2019-11-11 14:11:44 +0100

    made canyon more player-friendly

--- PodrozwNieznane/Assets/Scripts/GameManager.cs
+++ PodrozwNieznane/Assets/Scripts/GameManager.cs
@@ -110,6 +110,7 @@ public class GameManager : MonoBehaviour
         hexGrid.CreateMap();
         InitializePlayerUnit();
         //TODO: zwrócić uwagę czyja ma być kolej
+        hexGameUI.gameObject.SetActive(true);
         hexGameUI.SetSelectedUnit(players[activePlayer].HexUnit);
     }
 
@@ -123,7 +124,8 @@ public class GameManager : MonoBehaviour
         players[activePlayer].Character.LevelUp();
         mapPicker.ShowPicker(hexGrid.mapGenerator.GetLandscapeType()); //NextRound() is caled inside MapPicker
 
-        //TODO: make background inactive?
+        hexGrid.ClearPath();
+        hexGameUI.gameObject.SetActive(false);
     }
 
 }
